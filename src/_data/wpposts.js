deab773f1c7cd68c84d88cc3403468705a48d840
Michael D. Hilborn <mhilborn@biggles2k.com>
2023-04-23 14:54:33 -0400

    Paginated the eleventy-fetch of data from API.

--- src/_data/wpposts.js
+++ src/_data/wpposts.js
@@ -7,6 +7,7 @@ const cacheDuration  = "1d";
 const dataType       = "json";
 const orderBy        = "date";
 const order          = "desc";
+const postsPerQuery  = 5;
 
 // Use the below constants to define fetch options
 const eleventyFetchOptions = {
@@ -17,24 +18,38 @@ const eleventyFetchOptions = {
 
 // Query blog posts
 module.exports = async function() {
+    let fetchData      = [];
+    let finalData      = [];
+    let currentPage    = 1;
 
-    // Let's construct our query
-    let queryParamsObj = new URLSearchParams({
-        orderby: orderBy,
-        order: order
-    });
-    let queryParams    = queryParamsObj.toString();
-    let url            = `${wpEndPoint}?${queryParams}`;
-    let fetchData      = {};
-        
-    try {
-        // Fetch the query, which should be a JSON string
-        console.log(`Fetching URL: '${url}'`);
-        fetchData = await EleventyFetch(url, eleventyFetchOptions);
-        
-    } catch (err) {
-            console.log("Sadly, an error in Fetch occurred: " + err);
-    }
-    return fetchData;
+    do { 
+        // Let's construct our query
+        let queryParamsObj = new URLSearchParams({
+            orderby: orderBy,
+            order: order,
+            per_page: postsPerQuery,
+            page: currentPage
+        });
+        let queryParams    = queryParamsObj.toString();
+        let url            = `${wpEndPoint}?${queryParams}`;
+
+        try {
+            // Fetch the query, which should be a JSON string
+            console.log(`Fetching URL: '${url}'`);
+            fetchData = await EleventyFetch(url, eleventyFetchOptions);
+            
+        } catch (err) {
+                console.log("Sadly, an error in Fetch occurred: " + err);
+        }
+
+        // Push the fetchData into the final array
+        finalData.push(...fetchData);
+
+        // Increment the page offset
+        currentPage++;
+    } while ( fetchData.length == postsPerQuery);
+    
+    // Return the final array of data
+    return finalData;
 };
 
