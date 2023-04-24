991ae4a062e333894ab1ebb6a4f863dde83186db
Michael D. Hilborn <mhilborn@biggles2k.com>
2023-04-24 07:48:20 -0400

    Added serverless function to trigger GitHub Actions.

--- netlify/functions/wordpress-deploy.js
+++ netlify/functions/wordpress-deploy.js
@@ -1,8 +1,31 @@
-export const handler = async (event, context) => {
-   return {
-     statusCode: 200,
-     body: JSON.stringify({
-       message: 'Hello World!',
-     }),
-   }
- }
+require('dotenv').config();
+const EleventyFetch = require("@11ty/eleventy-fetch");
+
+// Set up constants used in the GitHub action request
+const gitHubEndpoint = process.env.GITHUB_ENDPOINT; 
+const fetchOptions   = {
+    "method":  "POST",
+    "body":    '{ "event_type": "wordpress" }',
+    "headers": {
+        "Accept": "application/vnd.github+json",
+        "Authorization": `token ${process.env.GITHUB_AUTHZ}`
+    }
+}
+
+exports.handler = async (event, context) => {
+    let json = {};
+    try {
+        console.log(`Fetching URL: ${gitHubEndpoint}`);
+        let response = await fetch(gitHubEndpoint, fetchOptions);
+        json         = await response.json();
+
+    } catch (err) {
+        console.log("Error in wordpress-deploy.js fetch: " + err);
+    };
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(json)
+    };
+}
+
